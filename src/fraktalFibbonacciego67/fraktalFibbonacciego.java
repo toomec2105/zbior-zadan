528fbf8346c259fba9ffca5f13893698ba655397
toomec2105 <toomec2105@interia.pl>
2020-08-18 19:21:37 +0200

    finish 67

--- src/fraktalFibbonacciego67/fraktalFibbonacciego.java
+++ src/fraktalFibbonacciego67/fraktalFibbonacciego.java
@@ -6,36 +6,40 @@ import java.util.List;
 public class fraktalFibbonacciego {
 	public static void main(String[] args) {
 		List<Integer> fibbonacci = getFibbonacciSequence();
-		List<String> bianryFibbonacci = new ArrayList<>();
-		for(Integer num : fibbonacci) {
+		List<String> binaryFibbonacci = new ArrayList<>();
+		for (Integer num : fibbonacci) {
 			String binary = "";
 			String mainPart = Integer.toBinaryString(num);
-			for(int i = 0 + mainPart.length(); i < 27; i++) {
+			for (int i = 0 + mainPart.length(); i < 27; i++) {
 				binary += "0";
 			}
 			binary += mainPart;
-			
-			bianryFibbonacci.add(binary);
+
+			binaryFibbonacci.add(binary);
 		}
 		/*
 		 * 67.1 System.out.println("F10 = " + fibbonacci.get(9) + " " + "F20 = " +
 		 * fibbonacci.get(19) + " " + "F30 = " + fibbonacci.get(29) + " " + "F40 = " +
 		 * fibbonacci.get(39) + " ");
 		 */
-		//67.2
-		//System.out.println("Expected: ??? Actual: " + getPrimeElements(fibbonacci));\
-		
-		/* 67.3
-		 * for(int i = 0; i< 39; i++) { System.out.println(bianryFibbonacci.get(i)); }
+		// 67.2
+		// System.out.println("Expected: ??? Actual: " + getPrimeElements(fibbonacci));\
+
+		/*
+		 * 67.3 for(int i = 0; i< 39; i++) {
+		 * System.out.println(bianryFibbonacci.get(i)); }
 		 */
-		
-		
+		for (int i = 0; i < binaryFibbonacci.size(); i++) {
+			if (countOnes(binaryFibbonacci.get(i)) == 6) {
+				System.out.println(binaryFibbonacci.get(i));
+			}
+		}
+
 	}
 
-
-	/* 67.2
-	 * private static List<Integer> getPrimeElements(List<Integer> fibbonacci) {
-	 * List<Integer> primes = new ArrayList<>();
+	/*
+	 * 67.2 private static List<Integer> getPrimeElements(List<Integer> fibbonacci)
+	 * { List<Integer> primes = new ArrayList<>();
 	 * 
 	 * for(Integer num : fibbonacci) { if(getDividers(num) == 1) { primes.add(num);
 	 * } }
@@ -55,6 +59,16 @@ public class fraktalFibbonacciego {
 	 * 
 	 * }
 	 */
+	public static int countOnes(String str) {
+		int count = 0;
+
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == '1')
+				count++;
+		}
+
+		return count;
+	}
 
 	private static List<Integer> getFibbonacciSequence() {
 		List<Integer> fibbonacci = new ArrayList<>();
